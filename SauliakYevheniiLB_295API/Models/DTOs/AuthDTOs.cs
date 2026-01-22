950d167d9415f9602618e4f54921bf10e6bb3f38
Yen Sauliak <znasku4@gmail.com>
2026-01-22 08:57:43 +0100

    User Model and AuthDTOs structure created

---
+++ SauliakYevheniiLB_295API/Models/DTOs/AuthDTOs.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SauliakYevheniiLB_295.Models.DTOs;
+
+public class LoginRequest
+{
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
+
+public class LoginResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+}
+
+public class RefreshRequest
+{
+    [Required]
+    public string RefreshToken { get; set; } = string.Empty;
+}
---
+++ SauliakYevheniiLB_295API/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SauliakYevheniiLB_295.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+}
