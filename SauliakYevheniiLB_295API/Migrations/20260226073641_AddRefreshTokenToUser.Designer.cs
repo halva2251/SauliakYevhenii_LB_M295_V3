42df8c7a315230ab38af2e8ad3e454e6fc11698b
Yen Sauliak <znasku4@gmail.com>
2026-02-26 09:04:09 +0100

    refresh token migration and postman test changes

--- SauliakYevheniiLB_295API/Migrations/20260226073641_AddRefreshTokenToUser.Designer.cs
+++ SauliakYevheniiLB_295API/Migrations/20260226073641_AddRefreshTokenToUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SauliakYevheniiLB_295API.Data;
 
@@ -11,9 +12,11 @@ using SauliakYevheniiLB_295API.Data;
 namespace SauliakYevheniiLB_295API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260226073641_AddRefreshTokenToUser")]
+    partial class AddRefreshTokenToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SauliakYevheniiLB_295API/Migrations/20260226073641_AddRefreshTokenToUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SauliakYevheniiLB_295API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "Users");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "PasswordHash", "Username" },
+                values: new object[] { 1, "$2a$11$HsXwOXfhvHwkqYK5tFfmDOuGlLQBU9B5V8H5jF5Y5Y5Y5Y5Y5Y5Y5", "admin" });
+        }
+    }
+}
